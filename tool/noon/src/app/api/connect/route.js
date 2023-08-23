2e6c8256eb92c00cdaf9317526a0f1164092f98b
Divyendra-S <65616112+Divyendra-S@users.noreply.github.com>
2023-08-23 14:46:52 +0000

    last commit

--- tool/noon/src/app/api/connect/route.js
+++ tool/noon/src/app/api/connect/route.js
@@ -3,20 +3,47 @@ import { connectDB } from "@/helper/db";
 import { User } from "@/models/user";
 import jwt from "jsonwebtoken";
 
-connectDB();
-
 export async function GET(req) {
-  const gettoken = req.cookies.get("authToken")?.value;
-  const verifytoken = jwt.verify(gettoken, process.env.JWT_SECRET);
   try {
+    // Ensure DB is connected for each request
+    connectDB();
+
+    // Get and verify the token
+    const gettoken = req.cookies.get("authToken")?.value;
+    const verifytoken = jwt.verify(gettoken, process.env.JWT_SECRET);
+
+    // Find user and return
     const user = await User.findById(verifytoken._id);
     return NextResponse.json(user);
   } catch (error) {
+    // Handle any error that might occur
     return NextResponse.json(
-      { message: error.message, success: true },
+      { message: error.message, success: false },
       {
         status: 400,
       }
     );
   }
 }
+// import { NextResponse } from "next/server";
+// import { connectDB } from "@/helper/db";
+// import { User } from "@/models/user";
+// import jwt from "jsonwebtoken";
+
+// connectDB();
+
+// export async function GET(req) {
+//   const gettoken = req.cookies.get("authToken")?.value;
+//   const verifytoken = jwt.verify(gettoken, process.env.JWT_SECRET);
+//   try {
+//     const user = await User.findById(verifytoken._id);
+//     return NextResponse.json(user);
+//   } catch (error) {
+//     return NextResponse.json(
+//       { message: error.message, success: false },
+//       {
+//         status: 400,
+//       }
+//     );
+//   }
+// }
